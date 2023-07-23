5d486150afc079e90b8aef8602b319ab9d0abc7a
Suraj Singh <kumarsurajsingh93@gmail.com>
2023-07-24 02:12:00 +0530

    added new api for particular product.

--- src/main/java/com/example/cicddemo/controller/ProductController.java
+++ src/main/java/com/example/cicddemo/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,9 @@ public class ProductController {
     @GetMapping("/")
     ResponseEntity<List<Product>> getAllProducts(){
         return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    ResponseEntity<Product> getProdcuts(@PathVariable Integer id){
+        return new ResponseEntity<>(productService.getProducts(id),HttpStatus.OK);
     }
 }
--- src/main/java/com/example/cicddemo/service/ProductService.java
+++ src/main/java/com/example/cicddemo/service/ProductService.java
@@ -4,15 +4,22 @@ import com.example.cicddemo.model.Product;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
+    private static List<Product> products = Arrays.asList(
+            new Product(1,"Mobile",35000.0),
+            new Product(2,"Laptop",65000.0),
+            new Product(3,"Tablet",45000.0)
+
+    );
     public List<Product> getAllProducts(){
-        List<Product> products = new ArrayList<>();
-        products.add(new Product(1,"Mobile",35000.0));
-        products.add(new Product(2,"Laptop",65000.0));
-        products.add(new Product(3,"Tablet",45000.0));
+
         return products;
+    }
+    public Product getProducts(Integer id){
+        return products.stream().filter(p->p.getProductId().equals(id)).findFirst().get();
     }
 }
